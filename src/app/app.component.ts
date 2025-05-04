ba18b9fe9345cb6f29bd9e09f2b7a4e2551168f3
Gian Luca da Silva Figueiredo <luca.gian16@gmail.com>
2025-05-04 11:17:55 -0300

    - incluir componente de mapas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { MapsComponent } from '@components';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [MapsComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-}
+export class AppComponent {}
---
+++ src/app/components/maps/maps.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  standalone: true,
+  selector: 'app-maps',
+  styleUrl: './maps.component.scss',
+  templateUrl: './maps.component.html',
+})
+export class MapsComponent {}
