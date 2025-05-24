0fc96eb70c1d8417b487a8eb36e15ff5f08f3e1b
Gian Luca da Silva Figueiredo <luca.gian16@gmail.com>
2025-05-24 18:54:41 -0300

    - apagar arquivos não necessários

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,14 +3,12 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimationsAsync(),
     provideHttpClient(withFetch()),
-    provideClientHydration(withEventReplay()),
     provideRouter(routes, withComponentInputBinding()),
     provideZoneChangeDetection({ eventCoalescing: true }),
   ],
