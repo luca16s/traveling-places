e4a0cf0fde93b4e871905c360fabcba64b84dced
Gian Luca da Silva Figueiredo <luca.gian16@gmail.com>
2025-05-25 08:57:28 -0300

    - corrigir nome da variável

--- src/app/shared/services/base.service.ts
+++ src/app/shared/services/base.service.ts
@@ -5,35 +5,40 @@ import { Observable } from 'rxjs';
 import { environment } from '@env';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BaseService {
-  constructor(
-    private http: HttpClient,
-    @Inject('url') private url: string,
-  ) { }
+  constructor(private http: HttpClient, @Inject('url') private url: string) {}
 
   get<TModel>(): Observable<TModel> {
-    return this.http.get<TModel>(`${environment.apiUrl}${this.url}`);
+    return this.http.get<TModel>(`${environment.baseApiUrl}${this.url}`);
   }
 
   getById<TModel, TId>(id: TId): Observable<TModel> {
-    return this.http.get<TModel>(`${environment.apiUrl}${this.url}/${id}`);
+    return this.http.get<TModel>(`${environment.baseApiUrl}${this.url}/${id}`);
   }
 
   post<TModel>(data: TModel): Observable<TModel> {
-    return this.http.post<TModel>(`${environment.apiUrl}${this.url}`, data);
+    return this.http.post<TModel>(`${environment.baseApiUrl}${this.url}`, data);
   }
 
   refresh(): Observable<boolean> {
-    return this.http.get<boolean>(`${environment.apiUrl}${this.url}/refresh`);
+    return this.http.get<boolean>(
+      `${environment.baseApiUrl}${this.url}/refresh`
+    );
   }
 
   update<TModel, TId>(id: TId, data: TModel): Observable<TModel> {
-    return this.http.put<TModel>(`${environment.apiUrl}${this.url}/${id}`, data);
+    return this.http.put<TModel>(
+      `${environment.baseApiUrl}${this.url}/${id}`,
+      data
+    );
   }
 
   patch<TModel, TId>(id: TId, data: TModel): Observable<TModel> {
-    return this.http.patch<TModel>(`${environment.apiUrl}${this.url}/${id}`, data);
+    return this.http.patch<TModel>(
+      `${environment.baseApiUrl}${this.url}/${id}`,
+      data
+    );
   }
 }
